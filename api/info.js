5e47e5d690a354e954282ed25d44e958d5747e92
Robbie Page <robbie.page@e-gineering.com>
2025-06-19 12:17:21 -0400

    revert for ARMA

--- api/info.js
+++ api/info.js
@@ -36,15 +36,15 @@ function calculateResponse(today, response) {
 
   if (day === 4) {
     response.days_until = 0;
-    response.message = 'IT IS ARMA NIGHT DAY BUT NOW ENSHROUDED';
+    response.message = 'IT IS ARMA NIGHT DAY';
   } else if (day === 3) {
     response.days_until = 1;
-    response.message = 'IT IS ARMA BUT NOW ENSHROUDED NIGHT DAY EVE';
+    response.message = 'IT IS ARMA NIGHT DAY EVE';
   } else if (day !== 4) {
     const days = (4 + (7 - day)) % 7;
 
     response.days_until = days;
-    response.message = `${days} DAYS UNTIL ARMA BUT NOW ENSHROUDED NIGHT DAY`;
+    response.message = `${days} DAYS UNTIL ARMA NIGHT DAY`;
   }
 
   return response;
@@ -85,8 +85,8 @@ async function calculateCountdown(today, utc_offset, response) {
   countdown.seconds_display = `${secondsZeroPad}${seconds}s`;
 
   countdown.countdown_text = (distance < 0)
-    ? 'SOAR OVER THE SHROUD!'
-    : `Yak about yaks in ${days}d ${hoursZeroPad}${hours}h ${minutesZeroPad}${minutes}m ${secondsZeroPad}${seconds}s`;
+    ? 'SQUAD, ASSEMBLE!'
+    : `Squad assembles in ${days}d ${hoursZeroPad}${hours}h ${minutesZeroPad}${minutes}m ${secondsZeroPad}${seconds}s`;
 
   response.countdown = countdown;
 
@@ -94,7 +94,7 @@ async function calculateCountdown(today, utc_offset, response) {
     const index = (distance < 0) ? 0 : hours + (utc_offset + 1);
     const location = cities[index].location;
 
-    response.eight_pm_in = `It is currently Arma but now Enshrouded night day time in ${location}.`
+    response.eight_pm_in = `It is currently Arma night day time in ${location}.`
   }
 
   return response;
